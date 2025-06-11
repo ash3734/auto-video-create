ca53d94cae16323d43b7be69e8809e02f02cd76b
ash <ash3734@gmail.com>
2025-06-11 17:10:46 +0900

    feat: FastAPI Lambda 배포를 위한 IaC 및 GitHub Actions 워크플로우 추가

--- auto_video_create_server/main.py
+++ auto_video_create_server/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from api.blog import router as blog_router
+from mangum import Mangum
 
 app = FastAPI()
 app.include_router(blog_router, prefix="/api/blog")
+
+handler = Mangum(app)
--- infra/main.tf
+++ infra/main.tf
@@ -68,4 +68,78 @@ resource "aws_s3_bucket" "test" {
 
 data "aws_caller_identity" "current" {}
 
-# 업로드/삭제는 AWS IAM 인증 사용자만 가능 (별도 정책 필요 없음) 
+# 업로드/삭제는 AWS IAM 인증 사용자만 가능 (별도 정책 필요 없음)
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "fastapi" {
+  function_name = "my-fastapi-backend"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "main.handler"
+  runtime       = "python3.11"
+  filename      = "${path.module}/../lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda.zip")
+
+  environment {
+    variables = {
+      ENV = "production"
+      # 필요한 환경변수 추가
+    }
+  }
+  timeout = 30
+  memory_size = 512
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "fastapi-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.fastapi.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "prod"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.fastapi.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  value = aws_apigatewayv2_stage.default.invoke_url
+} 
